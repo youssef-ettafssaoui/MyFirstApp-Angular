5596ccf800cde5e549be2572240e64ae5577d770
Yettafssaoui23 <yettafssaoui2@gmail.com>
2022-03-16 00:54:30 +0100

    Exporting Components

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -6,10 +6,15 @@ import { AuthModalComponent } from './auth-modal/auth-modal.component';
 
 @NgModule({
   declarations: [
+    // This component will be accessible from other components declared in the same module.
     AuthModalComponent
   ],
   imports: [
     CommonModule
+  ],
+  // The exports option is an array of components
+  exports: [
+    AuthModalComponent
   ]
 })
 export class UserModule { }
