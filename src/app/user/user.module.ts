75a1b6dd502a75b6d2d75ccd089ed478ada0e4c1
Yettafssaoui23 <yettafssaoui2@gmail.com>
2022-03-16 01:07:55 +0100

    Creating a Shared Module

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
 
+import { SharedModule } from '../shared/shared.module';
+
 
 
 @NgModule({
@@ -10,7 +12,8 @@ import { AuthModalComponent } from './auth-modal/auth-modal.component';
     AuthModalComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   // The exports option is an array of components
   exports: [
