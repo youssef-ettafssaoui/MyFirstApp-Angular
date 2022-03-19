e04b83c3eeac6d189072acb6a5330afd6a52b7d6
Yettafssaoui23 <yettafssaoui2@gmail.com>
2022-03-19 20:52:55 +0100

    Opening the Modal

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalService } from '../services/modal.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  // After injecting the service, we can use it in our component.
+  constructor(public modal: ModalService) { }
 
   ngOnInit(): void {
+  }
+
+  openModal($event: Event) {
+    // The event argument will be annotated with the event object inside this method
+    // The prevent default function will prevent the default behavior of the browser by calling this method
+    // Users will not unexpectedly be redirected to a different page.
+    $event.preventDefault()
+    this.modal.toggleModal()
   }
 
 }
--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -4,6 +4,17 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ModalService {
+  private visible = false
 
   constructor() { }
+
+  // Inside this method, we will return the visible property.
+  isModalOpen() {
+    return this.visible
+  }
+
+  // we will create a method for toggling the invisible property to find a method
+  toggleModal() {
+    this.visible = !this.visible
+  }
 }
