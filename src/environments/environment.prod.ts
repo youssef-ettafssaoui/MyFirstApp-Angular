d00c869dc7830ab275b01d834b4bc8a36bcc104a
Yettafssaoui23 <yettafssaoui2@gmail.com>
2022-03-28 12:06:37 +0000

    Importing Firebase & AngularFireModule

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+      apiKey: "AIzaSyCFZqYIa8EQ3fGb5pGPpY2u7HcLHSgPZ4c",
+      authDomain: "vsag-1f9ee.firebaseapp.com",
+      projectId: "vsag-1f9ee",
+      storageBucket: "vsag-1f9ee.appspot.com",
+      messagingSenderId: "396391730429",
+      appId: "1:396391730429:web:e686b035771aa637110294"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCFZqYIa8EQ3fGb5pGPpY2u7HcLHSgPZ4c",
+    authDomain: "vsag-1f9ee.firebaseapp.com",
+    projectId: "vsag-1f9ee",
+    storageBucket: "vsag-1f9ee.appspot.com",
+    messagingSenderId: "396391730429",
+    appId: "1:396391730429:web:e686b035771aa637110294"
+  }
 };
 
 /*
