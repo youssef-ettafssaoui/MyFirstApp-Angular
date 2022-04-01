91d0dc9df5e39a04e7248db0e216953fd9e84444
Yettafssaoui23 <yettafssaoui2@gmail.com>
2022-04-01 15:32:50 +0000

    Adding Firestore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { NavComponent } from './nav/nav.component';
 import { environment } from 'src/environments/environment';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore'
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { AngularFireAuthModule } from '@angular/fire/compat/auth';
     AppRoutingModule,
     UserModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    AngularFirestoreModule
 
   ],
   providers: [],
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-register',
@@ -8,7 +9,10 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-  constructor(private auth: AngularFireAuth) {}
+  constructor(
+    private auth: AngularFireAuth,
+    private db: AngularFirestore
+    ) {}
 
   inSubmission = false
 
@@ -64,7 +68,14 @@ export class RegisterComponent {
         email,
         password
       )
-      console.log(userCred)
+
+      await this.db.collection('users').add({
+          name: this.name.value,
+          email: this.email.value,
+          age: this.name.value,
+          phoneNumber: this.phoneNumber.value
+      })
+
     } catch(e) {
       console.error(e)
 
