6622a5712b256b45b3f16b873e65cff99372a2eb
youssef-ettafssaoui <yettafssaoui2@gmail.com>
2022-06-29 13:54:15 +0100

    adding event-blocker in upload html

--- src/app/shared/directives/event-blocker.directive.ts
+++ src/app/shared/directives/event-blocker.directive.ts
@@ -7,11 +7,8 @@ export class EventBlockerDirective {
 
   // Our goal of this directive is to block and events default behavior :
   @HostListener('drop', ['$event'])
+  @HostListener('dragover', ['$event'])
   public handleEvent(event: Event) {
     event.preventDefault()
-    // event.stopPropagation()  like an extra precaution
-
   }
-
-
 }
